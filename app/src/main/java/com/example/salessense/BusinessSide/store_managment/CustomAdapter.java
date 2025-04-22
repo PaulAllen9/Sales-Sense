b150971bc677fbcb1c76882f4cf34e2fbe19e4ff
PaulAllen09 <paul.j.allen9@gmail.com>
2025-04-21 19:28:07 -0500

    adding Product class file and item selection popup

--- app/src/main/java/com/example/salessense/BusinessSide/store_managment/CustomAdapter.java
+++ app/src/main/java/com/example/salessense/BusinessSide/store_managment/CustomAdapter.java
@@ -1,15 +1,22 @@
 package com.example.salessense.BusinessSide.store_managment;
 
+
+import android.app.Dialog;
 import android.content.Context;
-import android.util.Log;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.salessense.Product;
 import com.example.salessense.R;
 
 import java.util.ArrayList;
@@ -17,13 +24,11 @@ import java.util.ArrayList;
 public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHolder>{
 
     private Context context;
-    private ArrayList<String> texts;
-    private ArrayList<Integer> images;
+    private ArrayList<Product> products;
 
-    public CustomAdapter(Context context, ArrayList<String> texts, ArrayList<Integer> images) {
+    public CustomAdapter(Context context, ArrayList<Product> products) {
         this.context = context;
-        this.texts = texts;
-        this.images = images;
+        this.products = products;
     }
 
 
@@ -34,22 +39,48 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHold
     public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         LayoutInflater inflater = LayoutInflater.from(context);
         View view = inflater.inflate(R.layout.store_display_card, parent, false);
+        Dialog mDialog = new Dialog(context);
+        DisplayMetrics metrics = new DisplayMetrics();
+        ((AppCompatActivity) context).getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        int width = metrics.widthPixels;
+        int height = metrics.heightPixels;
 
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Log.d("Clcik Button","Clicked Button");
+                mDialog.setContentView(R.layout.product_popup_display_card);
+                mDialog.getWindow().setLayout(width,height);
+                mDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                mDialog.setCancelable(false);
+
+                ImageButton exit = mDialog.findViewById(R.id.closePopupButton);
+                exit.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        mDialog.dismiss();
+                    }
+                });
+
+
+
+                mDialog.show();
+
+            }
+        });
         return new MyViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(MyViewHolder holder, int position) {
-        Log.d("CustomAdapter", "Binding position: " + position);
-        Log.d("CustomAdapter", "Text: " + texts.get(position));
-        Log.d("CustomAdapter", "Image: " + images.get(position));
-        holder.textView.setText(String.valueOf(texts.get(position)));
-        holder.imageView.setImageResource(images.get(position));
+        holder.textView.setText(String.valueOf(products.get(position).getName()));
+        holder.imageView.setImageResource(products.get(position).getPicture());
+
     }
 
     @Override
     public int getItemCount() {
-        return texts.size();
+        return products.size();
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
